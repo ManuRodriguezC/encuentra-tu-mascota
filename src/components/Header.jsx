45473617db220fca750a2852cc572df5a6d51c98
M-Azariel <walter.azariel.moreno@gmail.com>
2024-10-03 23:22:35 -0300

    texto agregado dentro de la imagen y reubicado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,10 @@
-import { Nav } from "./Nav"
 import '../index.css'
 
-export const Header = () => {
-  return (
-    <header className="">
-      <Nav />
-      <div className="image">
-
-      </div>
-      <img src="/images/HeaderIMG.jpg" className="w-full h-96 object-cover brightness-50  lg:brightness-50 relative" >
-      </img>
-        <p className="absolute">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum nisi veritatis ad natus dolorem et omnis a ut sequi reprehenderit consectetur, deleniti hic molestiae? Debitis corrupti quo aperiam nostrum ex?</p>
-    </header>
-  )
+export const Header=()=>{
+    return(
+        <div className="flex justify-center items-center relative">
+            <img src="/images/HeaderIMG.jpg" className="w-full h-96 object-cover custom-brightness" />
+            <p className="absolute w-[50%] bottom-10 right-20 text-xl text-justify lg:text-3xl md:text-2xl transition-all duration-500 lg:w-[40%] ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum nisi veritatis ad natus dolorem et omnis a ut sequi reprehenderit consectetur, deleniti hic molestiae? Debitis corrupti quo aperiam nostrum ex?</p>
+        </div>
+    )
 }
